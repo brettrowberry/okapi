d7234c232a2d691af7de180886afd3bffe92768b
Brett Rowberry <brettrowberry@Kellens-MacBook-Pro.local>
2020-11-14 09:44:16 -0500

    minimal giraffe index.html

---
+++ Program.fs
@@ -0,0 +1,32 @@
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+let webApp =
+    choose [
+        route "/" >=> htmlFile "WebRoot/index.html"
+        ]
+
+let configureApp (app : IApplicationBuilder) =
+    // Add Giraffe to the ASP.NET Core pipeline
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    // Add Giraffe dependencies
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .Configure(configureApp)
+                    .ConfigureServices(configureServices)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
